f49e217729581e9f5b90e9e9de7676f90d7aeab3
Andrei Shydlouski <ephraimus@gmail.com>
2024-11-06 00:12:50 +0300

    add all other tasks

--- sharding-repl-cache/scripts/mongo-init.sh
+++ sharding-repl-cache/scripts/mongo-init.sh
@@ -6,17 +6,21 @@ docker-compose exec configsvr mongosh --port 27017 --eval 'rs.initiate({_id: "co
 
 # Инициализация репликационного набора для шардов
 echo "Инициализация репликационного набора для шардов"
-
-docker-compose exec shard1 mongosh --port 27018 --eval 'rs.initiate({_id: "shard1ReplSet", members: [{ _id : 0, host : "shard1:27018" }]})'
-docker-compose exec shard2 mongosh --port 27019 --eval 'rs.initiate({_id: "shard2ReplSet", members: [{ _id : 0, host : "shard2:27019" }]})'
+docker-compose exec shard1-1 mongosh --port 27018 --eval 'rs.initiate({_id: "shard1ReplSet", members: [{ _id : 0, host : "shard1-1:27018" },{ _id : 1, host : "shard1-2:27028" },{ _id : 2, host : "shard1-3:27038" }]})'
+docker-compose exec shard2-1 mongosh --port 27019 --eval 'rs.initiate({_id: "shard2ReplSet", members: [{ _id : 0, host : "shard2-1:27019" },{ _id : 1, host : "shard2-2:27029" },{ _id : 2, host : "shard2-3:27039" }]})'
 
 #Если быстрее - то не успевает собраться кластер
 sleep 15
 
 # Добавление шардов в кластер
 echo "Добавление шардов в кластер"
-docker-compose exec mongos mongosh --port 27020 --eval 'sh.addShard("shard1ReplSet/shard1:27018")'
-docker-compose exec mongos mongosh --port 27020 --eval 'sh.addShard("shard2ReplSet/shard2:27019")'
+docker-compose exec mongos mongosh --port 27020 --eval 'sh.addShard("shard1ReplSet/shard1-1:27018")'
+docker-compose exec mongos mongosh --port 27020 --eval 'sh.addShard("shard1ReplSet/shard1-2:27028")'
+docker-compose exec mongos mongosh --port 27020 --eval 'sh.addShard("shard1ReplSet/shard1-3:27038")'
+
+docker-compose exec mongos mongosh --port 27020 --eval 'sh.addShard("shard2ReplSet/shard2-1:27019")'
+docker-compose exec mongos mongosh --port 27020 --eval 'sh.addShard("shard2ReplSet/shard2-2:27029")'
+docker-compose exec mongos mongosh --port 27020 --eval 'sh.addShard("shard2ReplSet/shard2-3:27039")'
 
 sleep 5
 
